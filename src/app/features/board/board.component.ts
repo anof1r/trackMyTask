2d46e4b90d5f2f36885a9f80ed76fcd0b7863d9a
Denis Anofriev <denchik.anofriev@gmail.com>
2024-10-09 11:40:17 +0300

    Http client  setup

--- src/app/features/board/board.component.ts
+++ src/app/features/board/board.component.ts
@@ -4,6 +4,8 @@ import { NgFor } from '@angular/common';
 import { sections } from '../constants/constants';
 import { Section, Task } from '../types/types';
 import { TasksComponent } from "../tasks/tasks.component";
+import { BoardUiService } from './board-ui.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-board',
@@ -17,6 +19,10 @@ export class BoardComponent {
 
   protected sections = sections // refactor to api call
 
+  constructor(protected readonly boardUiService: BoardUiService) {
+    this.boardUiService.getTasks().pipe(tap(console.log)).subscribe()
+  }
+  
   get connectedLists(): string[] {
     return this.sections.map((_, index) => 'list' + index);
   }
