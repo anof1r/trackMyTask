dd33ea29d030c8c3cddbc49a72b7695c12f934c3
Denis Anofriev <denchik.anofriev@gmail.com>
2024-11-05 16:26:18 +0300

    styles lil fix

--- src/app/features/tasks-page/tasks-page.component.ts
+++ src/app/features/tasks-page/tasks-page.component.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
-import { MOCK_TABLE_DATA, TABLE_TASKS } from '../consts/consts';
 import { BoardApiService } from '../board/board-api.service';
 import { Task } from '../types/types';
 import { Observable } from 'rxjs';
 import { TagModule } from 'primeng/tag';
 import { StatusSeverityPipe } from './pipes/severity-pipe';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Dropdown, DropdownModule } from 'primeng/dropdown';
+import { DropdownModule } from 'primeng/dropdown';
 @Component({
   selector: 'app-tasks-page',
   standalone: true,
