fdcca3b422e6d6d12a4cc0b195c54074666f62e6
Denis Anofriev <denchik.anofriev@gmail.com>
2024-11-12 17:49:13 +0300

    more styles improvements

--- src/app/features/slider-menu/slider-menu.component.ts
+++ src/app/features/slider-menu/slider-menu.component.ts
@@ -1,11 +1,11 @@
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slider-menu',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './slider-menu.component.html',
   styleUrl: './slider-menu.component.scss'
 })
@@ -16,9 +16,16 @@ export class SliderMenuComponent {
     { label: 'Team', route: '/team', icon: 'group' }
   ];
 
+  isCollapsed = false;
+  currentRoute = '';
+
   constructor(private router: Router) { }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
+  }
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
   }
 }
--- src/app/features/task-menu/task-menu.component.ts
+++ src/app/features/task-menu/task-menu.component.ts
@@ -1,11 +1,11 @@
-import { NgClass, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Task } from '../types/types';
 
 @Component({
   selector: 'app-task-menu',
   standalone: true,
-  imports: [NgClass, NgIf],
+  imports: [NgClass, NgIf, NgFor],
   templateUrl: './task-menu.component.html',
   styleUrl: './task-menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -17,4 +17,15 @@ export class TaskMenuComponent {
   closeTaskInfo(): void {
     this.task = null;
   }
+  //TODO: move to custom pipe
+  statusClass(status: string): string {
+    switch (status) {
+      case 'TODO': return 'status-todo';
+      case 'IN PROGRESS': return 'status-inprogress';
+      case 'DONE': return 'status-done';
+      case 'IN REVIEW': return 'status-review';
+      default: return '';
+    }
+  }
+  
 }
