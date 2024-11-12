765189b2bb7964413c39bb433bb642526e7d5e71
Denis Anofriev <denchik.anofriev@gmail.com>
2024-11-12 20:07:31 +0300

    start of team page development

--- src/app/features/team-page/team-page.component.ts
+++ src/app/features/team-page/team-page.component.ts
@@ -1,13 +1,19 @@
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Card, CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-team-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor, CardModule],
   templateUrl: './team-page.component.html',
   styleUrl: './team-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TeamPageComponent {
-
+  teamMembers = [
+    { name: 'Denis', role: 'Frontend Developer', avatarUrl: 'assets/img/mm.png' },
+    { name: 'Alexandr', role: 'Backend Developer', avatarUrl: 'assets/img/mdd.jpg' },
+    { name: 'Oleg', role: 'Fullstack Developer', avatarUrl: 'assets/img/mdd.jpg' },
+  ];
 }
